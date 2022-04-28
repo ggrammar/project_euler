9ac5f12a7f8b0bc346bff3f207dd8919d4c92de3
Grant <grant.mattke.gordon@gmail.com>
2022-04-28 18:38:07 -0400

    solution for 1

--- 1.py
+++ 1.py
@@ -1,1 +1,15 @@
-print("hello")
+
+# "all natural numbers below 10", so not including 10. this is range's default behavior. 
+numbers = range(1, 1000000000)
+
+total = 0
+
+for i in numbers:
+    if (
+            (i % 3 == 0) or
+            (i % 5 == 0)):
+        total += i
+
+print total
+
+# This is correct. 
